af1ec6632ce927859ed6e5b3147fa90f73c3ccec
Majd Mawed <mawedmajd@gmail.com>
2023-02-19 14:36:24 +0100

    tested with postman the first part of the code

--- main.go
+++ main.go
@@ -7,49 +7,6 @@ import (
 	"os"
 )
 
-func ParseFile(filename string) []byte {
-	file, e := os.ReadFile(filename)
-	if e != nil {
-		fmt.Printf("File error: %v\n", e)
-		os.Exit(1)
-	}
-	return file
-}
-
-/*
-Responds with fixed JSON output sourced from provided file.
-*/
-func StubHandlerCountries(w http.ResponseWriter, r *http.Request) {
-
-	switch r.Method {
-	case http.MethodGet:
-		log.Println("Received " + r.Method + " request on Countries stub handler. Returning mocked information.")
-		w.Header().Add("content-type", "application/json")
-		output := ParseFile("./res/countries.json")
-		fmt.Fprint(w, string(output))
-		break
-	default:
-		http.Error(w, "Method not supported", http.StatusNotImplemented)
-	}
-}
-
-/*
-Responds with fixed JSON output sourced from provided file.
-*/
-func StubHandlerOccurrences(w http.ResponseWriter, r *http.Request) {
-
-	switch r.Method {
-	case http.MethodGet:
-		log.Println("Received " + r.Method + " request on Occurrences stub handler. Returning mocked information.")
-		w.Header().Add("content-type", "application/json")
-		output := ParseFile("./res/occurrences.json")
-		fmt.Fprint(w, string(output))
-		break
-	default:
-		http.Error(w, "Method not supported", http.StatusNotImplemented)
-	}
-}
-
 func main() {
 
 	// Define port
@@ -61,9 +18,9 @@ func main() {
 
 	// Standard http server with reference to stubbed handler
 	// If you want to adapt this, ensure to adjust path for compatibility with project
-	http.HandleFunc("/countries/no", StubHandlerCountries)
-	// Naturally, you can introduce multiple handlers to emulate different data sources
-	http.HandleFunc("/species/occurrences", StubHandlerOccurrences)
+	http.HandleFunc("/unisearcher/v1/uniinfo/", printhttp)
+	http.HandleFunc("/unisearcher/v1/neighbourunis/", printhttp)
+	http.HandleFunc("/unisearcher/v1/diag/", printhttp)
 
 	log.Println("Running on port", port)
 
@@ -72,4 +29,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+}
+
+func printhttp(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Working")
 }
